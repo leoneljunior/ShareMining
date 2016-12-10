e4a291b47d63b57154ab1b8f9e066b320f86e33e
leoneljunior <junior.hpj@hotmail.com>
2016-12-10 14:07:49 -0300

    [Leonel] Arquivos para configuração no Heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/app'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+// set the home page route
+app.get('/', function(req, res) {
+
+    // make sure index is in the right directory. In this case /app/index.html
+    res.render('index.html');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
