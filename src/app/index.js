61aabf68c61bdb4878a5302e55246b77c31d3ee7
leoneljunior <junior.hpj@hotmail.com>
2016-12-11 03:21:06 -0300

    no message

--- src/app/index.js
+++ src/app/index.js
@@ -39,7 +39,7 @@ angular.module('angularMaterialAdmin', ['ngAnimate', 'ngCookies', 'ngTouch',
         }
       });
 
-    $urlRouterProvider.otherwise('/graficos');
+    $urlRouterProvider.otherwise('/nuvens');
 
     $mdThemingProvider
       .theme('default')
