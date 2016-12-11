5d6603e010db9226a9a8730947c7c6f917553e4c
leoneljunior <junior.hpj@hotmail.com>
2016-12-11 02:49:32 -0300

    no message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/app'));
+// app.use(express.static(__dirname + '/app'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.engine('html', require('ejs').renderFile);
